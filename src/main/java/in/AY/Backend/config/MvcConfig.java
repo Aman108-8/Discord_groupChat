07bb5eb498718ede8d6aa87b7b82934efd02b0e1
Aman108-8 <aman704321@gmail.com>
2026-01-23 21:30:10 +0530

    mvcupdate

--- src/main/java/in/AY/Backend/config/MvcConfig.java
+++ src/main/java/in/AY/Backend/config/MvcConfig.java
@@ -3,6 +3,7 @@ package in.AY.Backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -24,7 +25,15 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
     }
-    
+
+	    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Allow your frontend to access backend
+        registry.addMapping("/**") // all endpoints
+                .allowedOrigins("http://localhost:5173") // frontend URL
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowCredentials(true);
+    }
     // You can add other WebMvcConfigurer methods here
 }
 
